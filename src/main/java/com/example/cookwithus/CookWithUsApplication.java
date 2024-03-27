06e9ba083efff8458dcefa2622e2b792936c012c
unknown <ts30164@seeu.edu.mk>
2024-03-27 02:52:57 +0100

    This is the first commit

---
+++ src/main/java/com/example/cookwithus/CookWithUsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookwithus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookWithUsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookWithUsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cookwithus/CookWithUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookwithus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookWithUsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
